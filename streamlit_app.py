3bde3f6a6626fabed700b3f35b7b96e6f48c79a5
Rupansh Verma <102577083+RupanshVerma@users.noreply.github.com>
2023-01-05 13:21:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title('My parents new healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Oatmeals')
+streamlit.text('Kale,Spinach & Rocket Smoothie')
+streamlit.text('Hard Boiled Egg')
