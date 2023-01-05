2191b1ae9416e98b2f53a76a5d25bb1fc1de3541
Rupansh Verma <102577083+RupanshVerma@users.noreply.github.com>
2023-01-05 13:31:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-import streamlit 
+import streamlit
+import pandas
 
 streamlit.title('New Healthy Dinner')
 streamlit.header('Healthy Breakfast Menu')
@@ -7,3 +8,6 @@ streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard Boiled Egg')
 streamlit.text('🥑🍞Avocade Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
